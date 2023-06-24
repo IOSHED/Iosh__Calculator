f3854c1de51cdf2d7f04c367fe3c6a95b6469033
IOSHED <iveninvala7@gmail.com>
2023-06-24 20:44:04 +0300

    rev

--- interpreter/target/debug/libi_calc.d
+++
@@ -1,1 +0,0 @@
-D:\developer\project\rust\calculator\i_calc\target\debug\libi_calc.rlib: D:\developer\project\rust\calculator\i_calc\src\ast.rs D:\developer\project\rust\calculator\i_calc\src\lib.rs
