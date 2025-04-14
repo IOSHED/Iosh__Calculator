16cd4413cfd78cf2219c2de33082c5465e281f89
IOSHED <iveninvala7@gmail.com>
2025-04-14 16:52:30 +0300

    Feat: View icon in .exe.

--- console/build.rs
+++ console/build.rs
@@ -1,5 +1,10 @@
 extern crate lalrpop;
+extern crate winres;
 
 fn main() {
     lalrpop::process_root().unwrap();
+
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("../resources/logo.ico");
+    res.compile().unwrap();
 }
